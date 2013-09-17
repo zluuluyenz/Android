68ebce4bdad238b1b6b21482492428f62b7d79db
Luyến <zluuluyenz@gmail.com>
2013-09-17 15:41:04 +0700

    ! Lingking Activities

--- Beginning Android™ 4 Application Development/UsingIntent/src/net/learn2develop/usingintent/MainActivity.java
+++ Beginning Android™ 4 Application Development/UsingIntent/src/net/learn2develop/usingintent/MainActivity.java
@@ -2,22 +2,27 @@ package net.learn2develop.usingintent;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
+	public void onClick(View view){
+		startActivity(new Intent("net.learn2develop.SecondActivity"));
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
